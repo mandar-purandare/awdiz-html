240cc59b0cc28b4217de6bb52e142d6aded6b9a2
mandar-purandare <mnpsstack@gmail.com>
2023-07-04 21:52:27 +0530

    create contact form with validation and link resume
    used anchor tag, many types of input and did password validation in js

---
+++ day 3/index.js
@@ -0,0 +1,17 @@
+function backToResume(){
+    // alert('Hi there, have a great day!');
+    window.location.assign('./../day 2/resume.html');
+}
+
+function showValuesAndValidate(event){
+    const name = document.querySelector('[name=name]').value;
+    const email = document.querySelector('[name=email]').value;
+    const password = document.querySelector('[name=password]').value;
+    if(password.length < 9 ){
+        alert("Password should be atleast 8 characters long ");
+        event.preventDefault();
+    }
+    else{
+        alert(`Thank you ${name}, your request is sent`);
+    }
+}
